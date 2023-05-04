55fad41b37c8dc2f605f18dea08164989d9a9ae1
laribox <laribox2014@gmail.com>
2023-05-04 07:30:24 +0100

    added a digital clock to the nav bar

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,11 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, AfterViewInit {
+  time: string = '';
   title: string = environment.title;
   icon: string = environment.icon;
+  ngAfterViewInit(): void {}
+  ngOnInit(): void {
+    const currentDate = new Date();
+    this.time = currentDate.toLocaleTimeString();
+    setInterval(() => {
+      const currentDate = new Date();
+      this.time = currentDate.toLocaleTimeString();
+    }, 1000);
+  }
 }
